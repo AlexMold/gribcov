fc85bcde48d6c70d93e3718cf81eb57b05b4c868
Alexandr Gribkov <alexandr.gribcov@listalpha.com>
2025-04-05 16:59:13 +0300

    fix: theme palette

--- apps/photo-converter/src/components/shared/Header.tsx
+++ apps/photo-converter/src/components/shared/Header.tsx
@@ -6,8 +6,8 @@ export const Header = () => {
   const { t } = useLanguage();
 
   return (
-    <header className="text-center mb-2">
-      <h1 className="display-5">{t('app.title')}</h1>
+    <header className="text-center mb-md-1">
+      <h1 className="display-5 mt-5 mt-md-0">{t('app.title')}</h1>
       <p className="lead text-muted">{t('app.description')}</p>
     </header>
   );
